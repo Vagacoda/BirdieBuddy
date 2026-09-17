591a14870a0bb51ea5c6684dd3be8b1de3e487ec
Vagacoda <gtavictsaven147@gmail.com>
2026-09-17 21:17:45 +0900

    2026/09/17 - 21:06 RolerEntity, UserEntity기초

--- src/main/kotlin/com/birdiebuddy/birdiebuddyapi/domain/user/UserEntity.kt
+++ src/main/kotlin/com/birdiebuddy/birdiebuddyapi/domain/user/UserEntity.kt
@@ -9,13 +9,14 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.PreUpdate
 import jakarta.persistence.Table
+import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDateTime
-import javax.accessibility.AccessibleState.ACTIVE
 
 // 2026/09/17 - 20:12 UserEntity 생성
 @Entity
-@Table
+@Table(name = "User")
 class UserEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,24 +29,51 @@ class UserEntity(
     // 클래스 안에서 권한 ID대신 RoleEntity 객체를 보관하는 필드, 실제 DB에 저장할때는 반드시 권한 필수
 
     @Column(nullable = false, unique = true, length = 100)
-    var email: String? = "",
+    var email: String = "",
     // email정보를 저장, 처음 생성시에는 null을 허용하나 DB에 저장될때는 NOT NULL이이기 때문에 유저에게는 반드시 email이 존재하여야 함
 
     @Column(nullable = false, unique = true, length = 30)
-    var nickname: String? = "",
+    var nickname: String = "",
     // 유저 이름을 저장, email과 유사함
 
     @Column(name = "password_hash", nullable = false, length = 255)
-    var passwordHash: String? = "",
+    var passwordHash: String = "",
     // 회원가입 시 Spring Security 해시값.
 
     @Column(nullable = false, length = 20)
     var status: String = "ACTIVE",
     // 회원 계정 상태. 가입 시 ACTIVE이며, 정지·탈퇴 처리에 사용함.
-    // ACTIVE, BLOCKED, WITHDRAW로 나뉨
+    // ACTIVE, BLOCKED, WITHDRAWN으로 구분
 
     @Column(name="email_verified_at")
     var emailVerifiedAt: LocalDateTime? = null,
-    // 이메일 인증시각은 email_verified로 저장
+    // 이메일 인증이 완료된 시각을 email_verified_at에 저장
 
-    )
+    @Column(name = "blocked_until")
+    var blockedUntil: LocalDateTime? = null,
+    // 유저 정지 시각을 blocked_until에 저장.
+    // 정상 회원과 영구 정지 회원은 null.
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    // 유저 회원가입 시각, 바뀌면 안되기 때문에 updatable = false임
+
+    @Column(name = "updated_at" , nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+    // 유저의 유저정보 수정 시각, 처음은 유저 회원가입 시각이며 이후 수정될때마다 갱신
+
+    @Column(name = "deleted_at")
+    var deletedAt: LocalDateTime? = null
+    // 유저 회원탈퇴시각, 회원탈퇴할때만 시간 저장.
+    ){
+    @PreUpdate
+    fun updatedModifiedTime(){
+        updatedAt = LocalDateTime.now()
+    }
+}
+
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun existsByEmail(email: String): Boolean
+    fun existsByNickname(nickname: String): Boolean
+    fun findByEmail(email: String): UserEntity?
+}
