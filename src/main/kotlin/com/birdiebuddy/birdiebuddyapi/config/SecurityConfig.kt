ea7f6b57be7940bbe87d664825d1aa105231db53
Vagacoda <gtavictsaven147@gmail.com>
2026-09-18 16:17:09 +0900

    2026/09/18 - 16:17 SecurityConfig작성

--- src/main/kotlin/com/birdiebuddy/birdiebuddyapi/config/SecurityConfig.kt
+++ src/main/kotlin/com/birdiebuddy/birdiebuddyapi/config/SecurityConfig.kt
@@ -22,13 +22,19 @@ class SecurityConfig { // 보안 설정 클래스
     // 어떤 APU에 누가 접근할 수 있는지 정하는 보안 규칙
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        // SpringSecurity에 접근규칙 생성, SpringBoot에 등록
+        // SpringSecurity에 접근규칙 생성, SpringBoot에 등록. HttpSecurity를 http변수명으로 사용
         http
             .csrf { it.disable() }
-            .authorizeHttpRequests { auth ->
+            // csrf보호기능 off, 추후 JWT(JsonWebToken)을 Authorization헤더에 넣어 보내는 API방식 사용
+            // 로그인한 사용자가 요청할때마다 로그인상태를 증명해야하기 때문에 번거롭지 않게 JWT를 사용.
+            .authorizeHttpRequests { auth -> // 주소별 접근 권한 설정이며, API 접근 권한을 정함
                 auth.requestMatchers("/api/users").permitAll()
+                // /api/users(회원가입 API주소) 는 비회원도 접근 가능.
                 auth.anyRequest().authenticated()
+                // 그 외 모든 주소는 JWT 등으로 로그인 인증을 통과한 회원만 접근가능.
             }
+        // http.csrf{}
+        // http.authorizeHttpRequests{}
         return http.build()
     }
 }
