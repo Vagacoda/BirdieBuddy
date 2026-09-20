97811e92b4967e2d956f2e9c28a8f6bae0bb1aea
Vagacoda <gtavictsaven147@gmail.com>
2026-09-20 16:38:52 +0900

    2026/09/19 - 16:38 LoginRequest 생성완료

--- src/main/kotlin/com/birdiebuddy/birdiebuddyapi/domain/auth/LoginRequest.kt
+++ src/main/kotlin/com/birdiebuddy/birdiebuddyapi/domain/auth/LoginRequest.kt
@@ -1,4 +1,18 @@
 package com.birdiebuddy.birdiebuddyapi.domain.auth
 
-class LoginRequest {
-}
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import jakarta.validation.constraints.Email
+
+// 2026/09/20 - 16:35 로그인서비스
+
+data class LoginRequest (
+    @field:NotBlank(message = "이메일은 필수 입력사항입니다.")
+    @field:Size(max = 254, message = "입력가능한 이메일 길이가 아닙니다.")
+    @field:Email(message = "올바른 이메일 형식이 아닙니다.")
+
+    val email: String,
+
+    @field:NotBlank(message = "비밀번호는 필수 입력사항입니다.")
+    val password: String,
+)
