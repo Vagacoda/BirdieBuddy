77602cc6372267b50daec4092258bd4565d7f61f
Vagacoda <gtavictsaven147@gmail.com>
2026-09-20 14:16:55 +0900

    2026/09/19 - 14:16 EmailTokenEntity에 이메일 인증 조회

--- src/main/kotlin/com/birdiebuddy/birdiebuddyapi/domain/email/EmailTokenEntity.kt
+++ src/main/kotlin/com/birdiebuddy/birdiebuddyapi/domain/email/EmailTokenEntity.kt
@@ -52,4 +52,19 @@ class EmailTokenEntity (
     //인증번호 생성 시각
     )
 
-interface EmailTokenRepository : JpaRepository<EmailTokenEntity, Long> {}
+interface EmailTokenRepository : JpaRepository<EmailTokenEntity, Long> {
+    fun findFirstByTargetEmailAndPurposeAndUsedAtIsNullOrderByCreatedAtDesc(
+        // 함수 이름 자체가 조회조건(Spring Data JPA제공)
+        //findFirstBy
+        //→ 조건에 맞는 행 중 하나를 찾음
+        //TargetEmailAndPurposeAndUsedAtIsNull
+        //→ 이메일이 같고
+        //→ 용도가 EMAIL_VERIFY이고
+        //→ 아직 사용하지 않은 used_at = NULL인 행
+        //OrderByCreatedAtDesc
+        //→ 생성 시각 최신순으로 정렬
+        //→ 가장 최근에 발급한 인증번호 한 건을 선택
+        targetEmail: String,
+        purpose: String
+    ): EmailTokenEntity?
+}
