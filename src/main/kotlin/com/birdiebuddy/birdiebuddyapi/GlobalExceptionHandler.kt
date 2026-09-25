7a47a807d1b4eba210a84ba73587b3552cd27d44
Vagacoda <gtavictsaven147@gmail.com>
2026-09-25 20:52:44 +0900

    2026/09/25 - 20:52 글로벌 예외 처리 주석

--- src/main/kotlin/com/birdiebuddy/birdiebuddyapi/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/birdiebuddy/birdiebuddyapi/GlobalExceptionHandler.kt
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 
 // 2026/09/25 - 19:28 전역 예외 처리 추가
-@RestControllerAdvice
+@RestControllerAdvice // 모든 Controller에서 발생한 오류를 JSON응답으로 처리
 class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgumentException(exception: IllegalArgumentException):
+            // 발생한 예외를 exception으로 받고 HTTP응답
             ResponseEntity<Map<String, String>> {
-        return ResponseEntity.badRequest().body(
+        return ResponseEntity.badRequest().body( // badRequest()는 HTTP 상태 코드 400 Bad Request를 의미
             mapOf(
                 "message" to (exception.message ?: "잘못된 요청입니다.")
             )
