451645fd73149870941932f7cc43253c5f99dd8c
Vagacoda <gtavictsaven147@gmail.com>
2026-09-25 19:41:23 +0900

    2026/09/25 - 19:41 글로벌 예외 처리 완성

--- src/main/kotlin/com/birdiebuddy/birdiebuddyapi/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/birdiebuddy/birdiebuddyapi/GlobalExceptionHandler.kt
@@ -1,8 +1,6 @@
 package com.birdiebuddy.birdiebuddyapi
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
